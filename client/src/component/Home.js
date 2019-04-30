e072421bc51ade76f880aee72c1fbb7bcf049dce
Jaxon Porter <jaxonutah@gmail.com>
2019-04-29 23:04:48 -0600

    added more content to home component

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -9,9 +9,16 @@ import logo from '../images/turtles.jpg'
     <>
       <Hero>
         <Disp>
-          <Text>I like Turtles.</Text>
+          <Text>I like Turtles...</Text>
         </Disp>
       </Hero>
+      <MidDiv>
+        <MidText></MidText>
+        <MidText>and coding.</MidText>
+      </MidDiv>
+      <BotDiv>
+        <BotText>and even more coding.</BotText>
+      </BotDiv>
     </>
   )
 }
@@ -33,6 +40,14 @@ const Text = styled.h1`
   color: white;
   font-size: 5em;
 `
+const MidText = styled.h1`
+  color: #ededed;
+  font-size: 5em;
+`
+const BotText = styled.h1`
+  color: #686868;
+  font-size: 5em;
+`
 
 const Disp = styled.div`
   display: flex;
@@ -40,4 +55,18 @@ const Disp = styled.div`
   width: 55vw;
   justify-content: center;
   align-items: center;
+`
+const MidDiv = styled.div`
+  height: 90vh; 
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: #686868;
+`
+const BotDiv = styled.div`
+  height: 90vh; 
+  display: flex;
+  padding-left: 10%;
+  align-items: center;
+  background: #ededed;
 `
