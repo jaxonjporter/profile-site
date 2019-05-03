c9f909f61006feca5b89bfbbed4f9f15bf9cd60a
Jaxon Porter <jaxonutah@gmail.com>
2019-05-03 13:48:53 -0600

    added static_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  Rails.application.routes.draw do
+    namespace :api do
+      resources :todos
+    end
+  
+    get '*other', to: 'static#index'
+  end
